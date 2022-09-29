0e908bea95faac837929ecb773525abf33c2ea56
Peter <peterng.918@gmail.com>
2022-09-29 14:57:01 -0400

    step1

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const Express = require('express');
+const Morgan = require('morgan');
+const layout = require("./views/layout");
+const app = Express();
+app.use(Express.static(__dirname + "/public"))
+app.get('/', (req, res) => {
+
+  res.send(layout(""));
+
+
+});
+
+const PORT = 1337;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});

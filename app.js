5e9c60a91b28b2407f972f4fff3d4d39fbc3aa08
fguillen1215 <111141844+fguillen1215@users.noreply.github.com>
2022-09-29 15:46:58 -0400

    step 2

--- app.js
+++ app.js
@@ -2,19 +2,32 @@
 const Express = require('express');
 const Morgan = require('morgan');
 const layout = require("./views/layout");
+const { db, Page, User } = require('./models');
+
+
 const app = Express();
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  })
 app.use(Express.static(__dirname + "/public"))
+app.use((Morgan('dev')))
 app.get('/', (req, res) => {
 
   res.send(layout(""));
 
 
 });
+async function init() {
+  // await Page.sync();
+  // await User.sync();
+  await db.sync()
+  
+  const PORT = 1337;
+  
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+}
+init();
 
-const PORT = 1337;
-
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
-
-'hello'
